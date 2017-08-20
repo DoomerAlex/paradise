0df6e83fdadba40366297b20c7d8ed714c75aa1c
DoomerAlex <DoomerAlex2@yandex.ru>
2017-08-20 03:38:58 +0300

    load Twig library

--- index.php
+++ index.php
@@ -5,9 +5,15 @@
 
 	require_once(ROOT.'/components/Router.php');
 	require_once(ROOT.'/components/DataBase.php');
+	require_once(ROOT.'/components/Twig/Autoloader.php');
 
 	//if (isset($_REQUEST[session_name()])) session_start();
 	session_start();
+
+	Twig_Autoloader::register();
+	$loader = new Twig_Loader_Filesystem('');
+	$twig = new Twig_Environment($loader);
+
 
 	$router = new Router();
 	$router->run();
